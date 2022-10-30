57305c06b6a92b54767a64dea44b65cb812b03fe
BetulioMay <cesarmay@protonmail.com>
2022-10-31 00:23:49 +0100

    Coneccion con la base de datos Oracle mediante cx_Oracle driver

--- src/run.py
+++ src/run.py
@@ -3,14 +3,42 @@ Programa principal.
 Aqui se ejecutara el `main loop` del programa
 '''
 
-from dotenv import *
-
+from dotenv import load_dotenv
 load_dotenv()
 
 import constants as c
 
+import cx_Oracle
+
 def run():
-	print("This is my HOST:", c.HOST)
+	try:
+		conn = cx_Oracle.connect(
+			user=c.USER,
+			password=c.PASSWD,
+			dsn=f"{c.HOST}:{c.PORT}/{c.DSN}",
+			encoding="UTF-8"
+		)
+		print(conn.version)
+	except Exception as e:
+		raise(e)
+
+	# Close connection
+	if conn:
+		conn.close()
 
 if __name__ == "__main__":
 	run()
+
+	# try:
+	# 	conn = pyodbc.connect(
+	# 		'DRIVER={Microsoft ODBC for Oracle};'
+	# 		f'Host={c.HOST};'
+	# 		f'Service Name={c.DSN};'
+	# 		f'User ID={c.USER};'
+	# 		f'Password={c.PASSWD};'
+	# 	)
+	# 	print(conn)
+	# 	conn.close()
+	# except Exception as e:
+	# 	print(f"ERROR: {e}")
+	# print(pyodbc.drivers())
