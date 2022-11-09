56a641cfbc3f19dcce27bcf4b9c7a8bf600ed009
adrianldga <101096281+adrianldga@users.noreply.github.com>
2022-11-09 17:12:20 +0100

    Añadido la funcion crear tablas

--- src/transactions/drop_insert.py
+++ src/transactions/drop_insert.py
@@ -13,3 +13,16 @@ def drop_tables(cursor) :
         return False
     return True
 
+def inster_tables(cursor) :
+    insert_stock = "create table stock (Cproducto int not null primary key,cantidad int default 0);"
+    insert_order = "create table pedido (Cpedido int not null primary key,Ccliente int not null,fecha_pedido date);"
+    insert_details = "create table detalle_pedido (Cproducto references stock(Cproducto),Cpedido references pedido(Cpedido),cantidad int default 1,primary key (Cproducto, Cpedido));"
+    try:
+        cursor.execute(insert_stock)
+        cursor.execute(insert_order)
+        cursor.execute(insert_details)
+    except Exception as e: 
+        print(e)
+        return False
+    return True
+
