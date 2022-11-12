8794afdbc9453b28b4aeb3cd729562cfb2f0f947
BetulioMay <cesarmay@protonmail.com>
2022-11-12 18:28:11 +0100

    forzar compresion zip

--- assignment.ps1
+++ assignment.ps1
@@ -1,3 +1,3 @@
 #!/usr/bin/env pwsh
 
-compress-archive -path ".\src",".\docs\informe.pdf",".\requirements.txt",".\run.ps1",".\.envrc" ".\entrega.zip" -compressionlevel optimal
+compress-archive -path ".\src",".\docs\informe.pdf",".\requirements.txt",".\run.ps1",".\.envrc" ".\entrega.zip" -compressionlevel optimal -Force
