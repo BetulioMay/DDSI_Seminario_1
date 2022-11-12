7dbe5f5445027322e04cc0e1f7bcf8d21ce8d2f9
adrianldga <101096281+adrianldga@users.noreply.github.com>
2022-11-12 15:11:13 +0100

    Versión final crear/borrar tablas

--- src/transactions/drop_insert.py
+++ src/transactions/drop_insert.py
@@ -15,11 +15,30 @@ def drop_tables(cursor) :
 
 def insert_tables(cursor) :
     insert_stock = "create table stock (Cproducto int not null primary key,cantidad int default 0)"
-    #insert_stock_tuplas = #Hacer luego"create table stock (Cproducto int not null primary key,cantidad int default 0)"
+    insert_stock_tuplas1 = "insert into stock values(134521,20)"
+    insert_stock_tuplas2 = "insert into stock values(224421,10)"
+    insert_stock_tuplas3 = "insert into stock values(352343,5)"
+    insert_stock_tuplas4 = "insert into stock values(242421,30)"
+    insert_stock_tuplas5 = "insert into stock values(678991,20)"
+    insert_stock_tuplas6 = "insert into stock values(546738,15)"
+    insert_stock_tuplas7 = "insert into stock values(298732,40)"
+    insert_stock_tuplas8 = "insert into stock values(846538,70)"
+    insert_stock_tuplas9 = "insert into stock values(982574,8)"
+    insert_stock_tuplas10 = "insert into stock values(172764,90)"
     insert_order = "create table pedido (Cpedido int not null primary key,Ccliente int not null,fecha_pedido date)"
     insert_details = "create table detalle_pedido (Cproducto references stock(Cproducto),Cpedido references pedido(Cpedido),cantidad int default 1,primary key (Cproducto, Cpedido))"
     try:
         cursor.execute(insert_stock)
+        cursor.execute(insert_stock_tuplas1)
+        cursor.execute(insert_stock_tuplas2)
+        cursor.execute(insert_stock_tuplas3)
+        cursor.execute(insert_stock_tuplas4)
+        cursor.execute(insert_stock_tuplas5)
+        cursor.execute(insert_stock_tuplas6)
+        cursor.execute(insert_stock_tuplas7)
+        cursor.execute(insert_stock_tuplas8)
+        cursor.execute(insert_stock_tuplas9)
+        cursor.execute(insert_stock_tuplas10)
         cursor.execute(insert_order)
         cursor.execute(insert_details)
     except Exception as e: 
